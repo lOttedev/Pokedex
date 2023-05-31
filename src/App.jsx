2d8d26c14b8d8ef1aeef03362c8bc4669a5910f7
Laurene Buttelli <laurene.buttelli@gmail.com>
2023-05-31 22:23:37 +0200

    try to code with props

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,32 @@ import MyTitle from "./components/MyTitle"
 import PokemonCard from "./components/PokemonCard"
 import "./App.css";
 
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+
 function App() {
 
+  const pokemon = pokemonList[0]
+
   return (
-
     <div>
-
-      <MyTitle />
-      <PokemonCard />
-
+    <MyTitle />
+  
+      <PokemonCard 
+        name={pokemon.name}
+        imgSrc={pokemon.imgSrc}
+        />
     </div>
-
 
   );
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,39 +1,28 @@
-function PokemonCard() {
-
-        const pokemon = pokemonList[0];
-
+function PokemonCard(props) {
+    const pokemon = props;
+  
     const pokidentity = () => {
       if (pokemon.imgSrc) {
         return pokemon.imgSrc;
-      } 
-      else {
+      } else {
         return "???";
       }
     };
-
+  
     return (
-        <div className="pokemon-card">
-          <figure className="card">
-            <img
-              src={pokidentity()}
-              alt= {pokemon.name + pokidentity()}
-              className="card-img"
-            />
-            <figcaption>{pokemon.name}</figcaption>
-          </figure>
-        </div>
-      );
-    }
-    const pokemonList = [
-        {
-          name: "bulbasaur",
-          imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-        },
-        {
-          name: "mew",
-        },
-      ];
-    
+      <div className="pokemon-card">
+        <figure className="card">
+          <img
+            src={pokidentity()}
+            alt={pokemon.name + pokidentity()}
+            className="card-img"
+          />
+          <figcaption>{pokemon.name}</figcaption>
+        </figure>
+      </div>
+    );
+  }
+  
+
     export default PokemonCard;
     
