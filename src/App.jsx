c02b63cc8f969f53d89c6c9080e46a5d0a21dbdf
Laurene Buttelli <laurene.buttelli@gmail.com>
2023-06-02 19:19:56 +0200

    add next and previous button to see others pokemon of our list

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,75 @@
 import MyTitle from "./components/MyTitle"
 import PokemonCard from "./components/PokemonCard"
+import { useState } from "react";
 import "./App.css";
 
 
 const pokemonList = [
+
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+  
+      name: "bulbasaur",
+  
+      imgSrc:
+  
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  
+    },
+  
+    {
+  
+      name: "charmander",
+  
+      imgSrc:
+  
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  
+    },
+  
+    {
+  
+      name: "squirtle",
+  
+      imgSrc:
+  
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  
+    },
+  
+    {
+  
+      name: "pikachu",
+  
+      imgSrc:
+  
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  
+    },
+  
+    {
+  
+      name: "mew",
+  
+    },
+  
+  ];
 
 
-function App() {
-
-  const pokemon = pokemonList[0]
+  function App() {
+    const [pokemonIndex, setPokemonIndex] = useState(0);
+    const pokemon = pokemonList[pokemonIndex];
+  
+    const handleClickPrevious = () => {
+      setPokemonIndex((prevIndex) =>
+        prevIndex === 0 ? pokemonList.length - 1 : prevIndex - 1
+      );
+    };
+  
+    const handleClickNext = () => {
+      setPokemonIndex((nextIndex) =>
+        nextIndex === pokemonList.length - 1 ? 0 : nextIndex + 1
+      );
+    };
 
   return (
     <div>
@@ -26,6 +78,8 @@ function App() {
       <PokemonCard 
         pokemon={pokemon}
         />
+  <button onClick={handleClickPrevious} type="button"> Précedent </button>
+  <button onClick={handleClickNext} type="button"> Suivant </button>
     </div>
 
   );
